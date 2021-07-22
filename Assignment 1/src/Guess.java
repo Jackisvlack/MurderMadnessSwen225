a9201f56108a3823ad4369910a2f4c04d0511c9a
Zeb <58132767+zebm@users.noreply.github.com>
2021-07-22 18:11:52 +1200

    Update Guess.java
    Added a guess class. This can be used to quickly compare a guess attempt with the murder set

--- Assignment 1/src/Guess.java
+++ Assignment 1/src/Guess.java
@@ -1,1 +1,13 @@
 
+class Guess {
+    String estate;
+    String weapon;
+    String character;
+
+    public Guess (String estate, String weapon, String character){
+        this.estate = estate;
+        this.weapon = weapon;
+        this.character = character;
+    }
+    
+}
