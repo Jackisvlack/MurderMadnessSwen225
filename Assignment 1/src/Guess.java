34750a6632002cac41b7d25063f2d940af2e2ac5
Zeb <58132767+zebm@users.noreply.github.com>
2021-07-22 18:12:53 +1200

    Update Guess.java
    Added Guess class. Easy comparison with a guess and the murder circumstances.

--- Assignment 1/src/Guess.java
+++ Assignment 1/src/Guess.java
@@ -4,10 +4,38 @@ class Guess {
     String weapon;
     String character;
 
+    
     public Guess (String estate, String weapon, String character){
         this.estate = estate;
         this.weapon = weapon;
         this.character = character;
     }
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Guess other = (Guess) obj;
+		if (character == null) {
+			if (other.character != null)
+				return false;
+		} else if (!character.equals(other.character))
+			return false;
+		if (estate == null) {
+			if (other.estate != null)
+				return false;
+		} else if (!estate.equals(other.estate))
+			return false;
+		if (weapon == null) {
+			if (other.weapon != null)
+				return false;
+		} else if (!weapon.equals(other.weapon))
+			return false;
+		return true;
+	}
     
 }
