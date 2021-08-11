61c038dd5b774c5aea59a0cf767a9431e64e762a
Zeb <58132767+zebm@users.noreply.github.com>
2021-08-12 09:17:53 +1200

    Double up problem

--- Assignment 1/src/Wall.java
+++
@@ -1,9 +0,0 @@
-/**
- * An instance of location with isWall set to true
- * */
-public class Wall extends Location{
-    public Wall(Position pos) {
-        super(pos);
-        super.isWall = true;
-    }
-}
